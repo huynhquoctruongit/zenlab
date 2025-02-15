834489730a336717814abfa36f872acdc4d942d9
Truonghq <huynhquoctruongit@gmail.com>
2025-02-15 19:22:07 +0700

    update UI homepage

--- src/components/share-layout/header.tsx
+++ src/components/share-layout/header.tsx
@@ -3,7 +3,7 @@ import Menu from './menu'
 
 export const Header = () => {
   return (
-    <div className='py-2 bg-white w-full sticky top-0 z-10 bg-white/50 backdrop-blur-md'>
+    <div className='py-2 bg-white w-full sticky top-0 z-10 bg-white/50 backdrop-blur-sm'>
       <div className='max-w-[1400px] px-6 mx-auto flex justify-between items-center'>
         <p className='font-bold text-2xl text-primary1'>ZenLab</p>
         <div className='flex items-center gap-4'>
